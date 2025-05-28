54f622b26e6fb87dfb7d91487fd70165cfc8494f
nubbot77 <cyclotron0707@gmail.com>
2025-05-28 14:13:01 +0530

    Deployment config

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np  
 import pandas as pd
 import dill
+
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.model_selection import GridSearchCV
 
@@ -58,7 +59,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models,params):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
